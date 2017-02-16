017bd6890e0dec240975e9e1b0029de211fa84c0
Ninger <ningerjohn@163.com>
2017-02-16 09:08:37 +0800

    change gitignore

---
+++ common/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'assetManager'=>[
+            'bundles'=>[
+                'yii\bootstrap\BootstrapAsset'=>[
+                    'sourcePath'=>'@common/assets/bootstrap', // bootstrap新样式文件
+                ]
+            ],
+        ],
+    ],
+];
